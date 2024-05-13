e681db745625dbd63a8f80f0117edcc576c19f01
Arturo Aragón <artuaragon.dev@gmail.com>
2024-05-13 22:36:24 +0200

    Solució Script python que liquidi els interessos anuals dels comptes de tipus 'C'.

--- LABS/Sessio 8/08transactions_triggers/script-s8.py
+++ LABS/Sessio 8/08transactions_triggers/script-s8.py
@@ -12,7 +12,7 @@ cursor = conn.cursor()
 cursor.execute("SELECT SUM(balance) FROM comptes WHERE type = 'C'")
 total_before = cursor.fetchone()[0]
 
-# Obtenim el saldo del compte del banc
+# Obtenim el saldo del compte del banc, que es un compte del tipus S amb el id mes petit de tots.
 cursor.execute("SELECT balance FROM comptes WHERE type = 'S' ORDER BY acc_id ASC LIMIT 1")
 bank_balance_before = cursor.fetchone()[0]
 
@@ -24,26 +24,26 @@ for row in cursor.fetchall():
     cursor.execute("UPDATE comptes SET balance = balance + ? WHERE type = 'S'", (interest,))
     cursor.execute("UPDATE comptes SET balance = balance - ? WHERE acc_id = ?", (interest, acc_id))
 
-# Obtenim el saldo total dels comptes de tipus 'C' després de la liquidació
-cursor.execute("SELECT SUM(balance) FROM comptes WHERE type = 'C'")
-total_after = cursor.fetchone()[0]
+print("\n - Suma total del balance dels comptes C abans de la transaccio: ", round(total_before, 2))
+print(" - Balance del banc abans de la transacció: ", round(bank_balance_before, 2))
 
-# Obtenim el saldo del compte del banc després de la transferència d'interessos
-cursor.execute("SELECT balance FROM comptes WHERE type = 'S' ORDER BY acc_id ASC LIMIT 1")
-bank_balance_after = cursor.fetchone()[0]
-
-# Verifiquem que la suma dels saldos sigui la mateixa abans i després de la liquidació
-assert round(total_before, 2) == round(total_after, 2), "La suma dels saldos dels comptes 'C' ha canviat després de la liquidació"
-assert round(bank_balance_before, 2) == round(bank_balance_after, 2), "El saldo del compte del banc ha canviat després de la liquidació"
-
-conn.commit()
-
-fer = input("Vols fer la transferencia? (SI/NO): ")
+fer = input("\nVols fer la transferencia? (SI/NO): ")
 
 if fer.upper() == "SI":
     # Confirmem els canvis a la base de dades
     conn.commit()
-    print("Liquidació d'interessos completada amb èxit.")
+
+    # Obtenim el saldo total dels comptes de tipus 'C' després de la liquidació
+    cursor.execute("SELECT SUM(balance) FROM comptes WHERE type = 'C'")
+    total_after = cursor.fetchone()[0]
+
+    # Obtenim el saldo del compte del banc després de la transferència d'interessos
+    cursor.execute("SELECT balance FROM comptes WHERE type = 'S' ORDER BY acc_id ASC LIMIT 1")
+    bank_balance_after = cursor.fetchone()[0]
+
+    print("\n - Suma total del balance dels comptes C despres de la transaccio: ", round(total_after, 2))
+    print(" - Balance del banc després de la transacció: ", round(bank_balance_after, 2))
+    print("\nLiquidació d'interessos completada amb èxit.")
 else:
     conn.rollback()
     print("Transferència cancel·lada.")
